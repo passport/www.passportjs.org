23f05e7416e9dcd5b7d8f7082351c5d3d4e93414
Ricardo Rauch <rickyrauch@gmail.com>
2015-04-28 12:42:49 -0300

    new stack

--- Makefile
+++
@@ -1,7 +0,0 @@
-site:
-	node site
-
-publish:
-	scp -rp passportjs.org jaredhanson@passportjs.org:/home/jaredhanson
-
-.PHONY: site publish
--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var server = express();
-
-server.use(express.logger());
-server.use(express.static('passportjs.org'));
-server.listen(3000);
