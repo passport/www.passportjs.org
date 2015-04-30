3c59c8584291bac35132ad48dd3e85692c325e55
Benjamin Flores <beneliflo@gmail.com>
2015-04-30 00:49:09 -0300

    Added docs-page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PassportJS' });
+});
+
+router.get('/docs', function(req, res, next) {
+  res.render('docs', { title: 'PassportJS - Documentation' });
 });
 
 module.exports = router;
