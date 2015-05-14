9ec0459e8459f0791c59a244aa3a43746dc0c570
Jared Hanson <jaredhanson@gmail.com>
2015-05-14 09:58:56 -0700

    Fetch package info from NPM when building search.

--- buildsearch.js
+++ buildsearch.js
@@ -3,6 +3,14 @@ var fs = require('fs');
 var githubUrl = require('github-url-from-git');
 var gh = require('github-url-to-object');
 var request = require('request');
+var NPMRegistry = require('npm-registry');
+
+var npm = new NPMRegistry();
+
+
+// TODO: Use something like this to programatically fetch all Passport strategies,
+// rather than building a manual list.
+// https://github.com/todvora/variety-plugins-searcher
 
 
 try {
@@ -28,21 +36,30 @@ try {
     var url = githubUrl(strategy.repository);
     var obj = gh(url);
     
-    request.get(obj.api_url,
-                { headers: { 'User-Agent': 'node' }, json: true }, function (err, resp, json) {
+    var name = strategy.name || obj.repo;
+    
+    npm.packages.get(name, function (err, npmData) {
       if (err) { return iter(err); }
-      if (resp.statusCode !== 200) {
-        console.log('Failed to fetch from GitHub: ' + obj.api_url);
-        json = {};
-      }
       
-      data.push({
-        label: obj.repo,
-        url: url,
-        stars: json.stargazers_count,
-        forks: json.forks_count
+      var pkg = npmData[0];
+    
+      request.get(obj.api_url,
+                  { headers: { 'User-Agent': 'node' }, json: true }, function (err, resp, json) {
+        if (err) { return iter(err); }
+        if (resp.statusCode !== 200) {
+          console.log('Failed to fetch from GitHub: ' + obj.api_url);
+          json = {};
+        }
+      
+        data.push({
+          label: obj.repo,
+          desc: pkg.description,
+          url: url,
+          stars: json.stargazers_count,
+          forks: json.forks_count
+        });
+        iter();
       });
-      iter();
     });
     
   })();
