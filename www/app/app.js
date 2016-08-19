f667d36f21349907646aecfc4c8b774450703a38
Jared Hanson <jaredhanson@gmail.com>
2016-08-19 15:48:05 -0700

    Add syntax highlighting.

--- www/app/app.js
+++ www/app/app.js
@@ -1,5 +1,27 @@
-define(['jquery'],
-function($) {
-  $(function() {
+define(['jquery', 'highlight'],
+function($, hljs) {
+
+  $(document).ready(function() {
+    
+    // homepage
+    function numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
+    $.getJSON('/repo.json', function(data) {
+      $(".social .stat").text(numberWithCommas(data.stargazers_count));
+    });
+  
+  
+    // docs
+    hljs.configure({ classPrefix: '' });
+  
+    // reset syntax highlight
+    $('pre code').each(function (i, block) {
+      hljs.highlightBlock(block);
+    });
+  
+  
   });
+
 });
--- www/main.js
+++ www/main.js
@@ -2,7 +2,8 @@ require.config({
   baseUrl: 'js/lib',
   paths: {
     'app': '../app',
-    'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min'
+    'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min',
+    'highlight': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/highlight.min'
   }
 });
 
