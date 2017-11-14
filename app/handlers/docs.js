9c5a40e223a267c801276ee5bc972d1ddbdb86df
Jared Hanson <jaredhanson@gmail.com>
2017-11-14 12:09:54 -0800

    Docs service responds with static generated page.

--- app/handlers/docs.js
+++ app/handlers/docs.js
@@ -2,7 +2,7 @@ exports = module.exports = function(showHandler) {
   var express = require('express');
   var router = new express.Router();
   
-  router.get('/:document?', showHandler);
+  router.get('/:slug?', showHandler);
   
   return router;
 };
--- app/handlers/docs/show.js
+++ app/handlers/docs/show.js
@@ -1,4 +1,7 @@
 exports = module.exports = function() {
+  var path = require('path')
+    , fs = require('fs');
+
   
   function getDocuments() {
     var path = require('path');
@@ -18,8 +21,23 @@ exports = module.exports = function() {
   var documents = getDocuments();
   
   
+  var baseDir = '../../../www/docs';
   
   function respond(req, res, next) {
+    var slug = req.params.slug
+      , file;
+    
+    if (slug) {
+      file = path.resolve(__dirname, baseDir, slug, 'index.html');
+      if (!fs.existsSync(file)) { return next(); }
+    }
+    
+    file = path.resolve(__dirname, '../../../www/docs/downloads/html/index.html');
+    return res.sendFile(file);
+  }
+  
+  
+  function respond_old(req, res, next) {
     if (req.params.document) {
       if (!~documents.indexOf(req.params.document)) return next();
       res.locals.context.canonical = '/docs';
