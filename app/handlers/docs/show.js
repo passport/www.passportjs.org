c1c125276aebc8e7170eae955ad0f2f76fcbd8d3
Jared Hanson <jaredhanson@gmail.com>
2017-11-14 13:18:34 -0800

    Remove dead code.

--- app/handlers/docs/show.js
+++ app/handlers/docs/show.js
@@ -1,24 +1,6 @@
 exports = module.exports = function() {
   var path = require('path')
     , fs = require('fs');
-
-  
-  function getDocuments() {
-    var path = require('path');
-    var resolve = path.resolve;
-    var fs = require('fs');
-    var read = fs.readdirSync;
-
-    var documents = read(resolve(__dirname, '../../../views/docs'));
-
-    return documents.filter(function (d) {
-      return /\.md$/.test(d);
-    }).map(function (d) {
-      return d.replace(/\.md$/, '');
-    });
-  }
-  
-  var documents = getDocuments();
   
   
   var baseDir = '../../../www/docs';
@@ -34,16 +16,6 @@ exports = module.exports = function() {
     
     file = path.resolve(__dirname, '../../../www/docs/downloads/html/index.html');
     return res.sendFile(file);
-  }
-  
-  
-  function respond_old(req, res, next) {
-    if (req.params.document) {
-      if (!~documents.indexOf(req.params.document)) return next();
-      res.locals.context.canonical = '/docs';
-    }
-    
-    res.render('docs', { title: 'Documentation', page_class: 'page-docs'});
   }
   
   return [ respond ];
