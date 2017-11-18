8bd6e99af0105da1a032fcabd4fe56d4b4c3a0b9
Jared Hanson <jaredhanson@gmail.com>
2017-11-17 16:35:15 -0800

    Update web app build.

--- web/build.js
+++ web/build.js
@@ -1,7 +1,7 @@
 ({
-    baseUrl: "public/js",
+    baseUrl: "./scripts",
+    name: "main",
     paths: {
-      'app': './app',
       'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min',
       'bloodhound': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min',
       'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min',
@@ -9,6 +9,5 @@
       'jquery.typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.jquery',
       'page': '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page'
     },
-    name: "main",
-    out: "public/js/main-built.js"
+    out: "../www/assets/scripts/main.js"
 })
--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,7 +1,5 @@
 require.config({
-  baseUrl: '/scripts/lib',
   paths: {
-    'app': '../app',
     'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min',
     'bloodhound': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min',
     'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min',
