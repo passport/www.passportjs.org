8d0e485a49639ea1a4105718d92f53b39ad36aa8
Jared Hanson <jaredhanson@gmail.com>
2017-11-18 15:25:50 -0800

    Clean up network activity status.

--- web/scripts/app/shell/status.js
+++ web/scripts/app/shell/status.js
@@ -1,77 +1,38 @@
 define(['exports', 'jquery', 'jquery.pjax'], function(exports, $, __$_pjax) {
   
   $(document).ready(function() {
-    //$('body').toggleClass('pjax-loading', true);
-    //return
-    
-    $(document).on('pjax:start', function () {
-      console.log('pjax:start');
-    });
-    
     $(document).on('pjax:send', function () {
-      console.log('pjax:send');
       exports.activity(true);
-    });
-    
-    $(document).on('pjax:complete', function () {
-      console.log('pjax:complete');
-      exports.activity(false);
-    });
-    
-    $(document).on('pjax:end', function () {
-      console.log('pjax:end');
-    });
-    
-    $(document).on('pjax:click', function () {
-      console.log('pjax:click');
-      togglePjaxLoading(true);
     });
 
     $(document).on('pjax:beforeReplace', function () {
-      console.log('pjax:beforeReplace');
-      togglePjaxLoading(false);
+      exports.activity(false);
     });
-    
   });
   
   
-  var _timeoutID = 0;
+  var _tid = 0;
   
   exports.activity = function(flag) {
-    console.log('ACTIVITY? ' + flag);
-    console.log(_timeoutID)
-    
     // do not add pjax-loading flag twice
     // wait for it to resolve or be canceled
-    if (flag && _timeoutID) {
+    if (flag && _tid) {
       return;
     }
     
-  };
-  
-  
-  var loadingTimeout = 0;
-  
-  function togglePjaxLoading(toggle) {
-    // do not add pjax-loading flag twice
-    // wait for it to resolve or be canceled
-    if (toggle && loadingTimeout) {
-      return;
-    }
-
     // cancel timer if toggle false
     // and remove pjax-loading flag
-    if (!toggle) {
-      clearTimeout(loadingTimeout);
+    if (!flag) {
+      clearTimeout(_tid); _tid = 0;
       $('body').toggleClass('pjax-loading', false);
       return;
     }
-
+    
     // save timeout timer and await to add
     // pjax-loading flag
-    loadingTimeout = setTimeout(function () {
+    _tid = setTimeout(function () {
       $('body').toggleClass('pjax-loading', true);
     }, 300);
-  }
+  };
   
 });
