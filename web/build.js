be2bab9667788b24394ad467374567a8e1737e4a
Jared Hanson <jaredhanson@gmail.com>
2017-11-20 17:20:47 -0800

    Update requirejs paths.

--- web/build.js
+++ web/build.js
@@ -1,17 +1,17 @@
 ({
-    baseUrl: "./script",
-    name: "main",
+    baseUrl: './script',
+    name: 'main',
     paths: {
       'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min',
-      'bloodhound': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min',
-      'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min',
       'jquery.pjax': '//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min',
-      'jquery.typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.jquery',
-      'page': '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page'
+      'jquery.typeahead': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.jquery.min',
+      'bloodhound': '//cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min',
+      'highlight': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min',
+      'page': '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min'
     },
     packages: [
       { name: 'class', main: 'class', location: '../node_modules/class' },
       { name: 'events', location: '../node_modules/events' }
     ],
-    out: "../www/script/main.js"
+    out: '../www/script/main.js'
 })
