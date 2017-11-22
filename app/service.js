297f8dfdf73b4f077f3a40b6823ccf74425cf589
Jared Hanson <jaredhanson@gmail.com>
2017-11-21 19:30:55 -0800

    WIP: Switch to express-simple-redirect.

--- app/service.js
+++ app/service.js
@@ -1,12 +1,12 @@
 exports = module.exports = function(repoHandler, logging) {
   var express = require('express');
-  var redirect = require('express-redirect');
+  var redirect = require('express-simple-redirect');
   var bodyParser = require('body-parser');
   var cookieParser = require('cookie-parser');
   var path = require('path');
   
   
-  var service = redirect(express());
+  var service = express();
   
   service.set('views', path.join(__dirname, '../views'));
   service.set('view engine', 'jade');
@@ -34,8 +34,39 @@ exports = module.exports = function(repoHandler, logging) {
   });
   
   // setup redirects
-  service.redirect('/guide', '/docs', 301);
-  service.redirect('/guide/:page', '/docs/:page', 301);
+  //service.redirect('/guide', '/docs', 301);
+  //service.redirect('/guide/:page', '/docs/:page', 301);
+  
+  service.use(redirect({
+    // TODO: Describe this ste.
+    '/guide/authenticate.html': '/docs/authenticate/',
+    '/guide/authorize.html': '/docs/authorize/',
+    '/guide/configuration.html': '/docs/configure/',
+    '/guide/facebook.html': '/docs/facebook/',
+    '/guide/google.html': '/docs/google/',
+    '/guide/log-out.html': '/docs/logout/',
+    '/guide/oauth-providers.html': '/docs/providers/',
+    '/guide/oauth.html': '/docs/oauth/',
+    '/guide/openid-providers.html': '/docs/providers/',
+    '/guide/openid.html': '/docs/openid/',
+    '/guide/twitter.html': '/docs/twitter/',
+    '/guide/user-profile.html': '/docs/profile/',
+    '/guide/username-password.html': '/docs/username-password/',
+    // TODO: Describe this set.
+    '/guide/authenticate/': '/docs/authenticate/',
+    '/guide/authorize/': '/docs/authorize/',
+    '/guide/configuration/': '/docs/configure/',
+    '/guide/facebook/': '/docs/facebook/',
+    '/guide/google/': '/docs/google/',
+    '/guide/log-out/': '/docs/logout/',
+    '/guide/oauth-providers/': '/docs/providers/',
+    '/guide/oauth/': '/docs/oauth/',
+    '/guide/openid-providers/': '/docs/providers/',
+    '/guide/openid/': '/docs/openid/',
+    '/guide/twitter/': '/docs/twitter/',
+    '/guide/user-profile/': '/docs/profile/',
+    '/guide/username-password/': '/docs/username-password/'
+  }, 301));
   
   
   service.get('/repo.json', repoHandler);
