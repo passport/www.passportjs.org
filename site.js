e56b57c1aeae7348692f5d3b59e92154ad27707e
Jared Hanson <jaredhanson@gmail.com>
2017-11-21 18:12:58 -0800

    Add title to docs.

--- site.js
+++ site.js
@@ -5,6 +5,13 @@ site.set('base url', 'http://www.passportjs.org');
 site.set('layout engine', 'pug');
 
 site.locals.pretty = true;
+/*
+site.locals.site = {
+  accounts: {
+    'twitter.com': 'passportjs'
+  }
+}
+*/
 
 //site.engine('jade', require('jade'));
 site.engine('pug', require('pug'));
@@ -12,7 +19,7 @@ site.engine('pug', require('pug'));
 //site.use('/packages', require('kerouac-npm-packages')());
 site.content('content');
 //site.content('views', { layout: 'docs' });
-site.use('/docs', require('kerouac-book')('docs', { layout: 'docs' }));
+site.use('/docs', require('kerouac-book')('Documentation', 'docs', { layout: 'docs' }));
 
 site.use('/', require('kerouac-manifest')());
 
