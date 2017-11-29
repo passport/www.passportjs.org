4f64a43b2a7600af4a3c23932821b49ed4d3816e
Jared Hanson <jaredhanson@gmail.com>
2017-11-29 10:20:14 -0800

    Cleanup

--- web/script/search/packages/remote/transport/all.js
+++ web/script/search/packages/remote/transport/all.js
@@ -12,33 +12,6 @@ define(['bloodhound', '../../sort', 'jquery'], function(Bloodhound, sort, $) {
   });
   
   
-  var _fetched = false;
-  
-  var loadPromise;
-  
-  function _loadRemote() {
-    var deferred;
-  }
-  
-  function remote(url) {
-    console.log('-- initi remote');
-    console.log(url);
-    
-    url = url || '/packages/-/v1/all.json';
-    
-    return $.ajax({ url: url, type: 'GET', dataType: 'json' })
-      .then(function(data, textStatus, request) {
-        console.log('done fetch');
-        
-        if (data.urls && data.urls.next) {
-          console.log('need to fetch: ' + data.urls.next);
-          return remote(data.urls.next)
-        }
-        
-      });
-  }
-  
-  
   var loadPromise;
   
   return function(options, onSuccess, onError) {
@@ -50,18 +23,7 @@ define(['bloodhound', '../../sort', 'jquery'], function(Bloodhound, sort, $) {
     
     engine.initialize()
       .then(load)
-      .done(function() {
-        console.log('-- remote loaded');
-        
-        engine.search(options.query, function(datums) {
-          //onSuccess(datums);
-          
-          console.log('GOT DATUMS');
-          console.log(datums);
-          
-          onSuccess(datums);
-        });
-      })
+      .done(search)
       .fail(function(){});
     
     
@@ -70,6 +32,8 @@ define(['bloodhound', '../../sort', 'jquery'], function(Bloodhound, sort, $) {
       return $.ajax(options).then(process);
       
       function process(data, textStatus, request) {
+        engine.add(data.objects);
+        
         if (data.urls && data.urls.next) {
           options.url = data.urls.next;
           return _load(options);
@@ -82,58 +46,14 @@ define(['bloodhound', '../../sort', 'jquery'], function(Bloodhound, sort, $) {
       return loadPromise;
     }
     
-    return;
-    
-    //console.log('TRANSPORT!');
-    //console.log(options);
-    
-    //var objects = [];
-    
-    if (_fetched) { return search(options.query); }
-    
-    engine.initialize()
-      .done(function() {
-        console.log('ENGINE INITIALIZED');
-        fetch(options.url);
+    function search() {
+      engine.search(options.query, function(datums) {
+        console.log('GOT DATUMS');
+        console.log(datums);
+        
+        onSuccess(datums);
       });
-    
-    //fetch(options.url);
-    
-    function search(query) {
-      engine.search(options.query,
-        function(datums) {
-          onSuccess(datums);
-        });
     }
-    
-    function fetch(url) {
-      var opts = { url: url, type: 'GET', dataType: 'json' };
-      
-      $.ajax(opts)
-        .done(function(data, textStatus, request) {
-          //console.log('DONE!');
-          //console.log(textStatus);
-          //console.log(data)
-          
-          engine.add(data.objects);
-          
-          //objects = objects.concat(data.objects);
-          if (data.urls && data.urls.next) {
-            fetch(data.urls.next);
-          } else {
-            //console.log('GOT EM ALL!');
-            //console.log(objects.length);
-            //onSuccess(objects);
-            
-            _fetched = true;
-            search(options.query);
-          }
-          
-          //onSuccess(data);
-        })
-        .fail(function(request, textStatus, errorThrown) { onError(errorThrown); });
-    }
-    
   };
   
 });
