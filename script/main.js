9d2aa336502b9b41b9a0c1aae13bee960cfa5bc0
Jared Hanson <jaredhanson@gmail.com>
2018-01-09 20:55:48 -0800

    Search results display download counts, rather than forks.

--- script/main.js
+++ script/main.js
@@ -375,7 +375,7 @@ define("search/packages/engine", [ "bloodhound", "./sort", "./remote/api-v1/all"
 
 define("search/packages/templates/result", [], function() {
     return function(item) {
-        return "<article" + (item._featured ? ' class="featured"' : "") + '><a href="' + item.homepage + '" target="_blank"><span class="title">' + item.name + '</span><span class="text">' + item.description + "</span>" + (item._featured ? '<span class="featured-flag">Featured</span>' : "") + '<span class="stat"><span class="download">' + (item.repository ? item.repository.forkCount || 0 : 0) + '</span><span class="star">' + (item.repository ? item.repository.favoriteCount || 0 : 0) + "</span></span></a></article>";
+        return "<article" + (item._featured ? ' class="featured"' : "") + '><a href="' + item.homepage + '" target="_blank"><span class="title">' + item.name + '</span><span class="text">' + item.description + "</span>" + (item._featured ? '<span class="featured-flag">Featured</span>' : "") + '<span class="stat"><span class="download">' + (item.downloads ? (item.downloads["last-week"] || 0).toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,") : 0) + '</span><span class="star">' + (item.repository ? item.repository.favoriteCount || 0 : 0) + "</span></span></a></article>";
     };
 });
 
