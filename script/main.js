b8b331d173c681076d8805378829468df5131d66
Jared Hanson <jaredhanson@gmail.com>
2018-01-27 19:35:32 -0800

    Clean up menu trigger stylesheets.

--- script/main.js
+++ script/main.js
@@ -397,7 +397,7 @@ define("shell/menu", [ "exports", "jquery" ], function(exports, $) {
     exports.toggle = function(open) {
         $("html").toggleClass("is-menu", open);
         $("#toolbar, #content").toggleClass("blured", open);
-        $(".menu-trigger").toggleClass("is-active", open).next().toggleClass("is-active", open);
+        $(".menu-trigger").toggleClass("is-open", open).next().toggleClass("is-active", open);
     };
     $(document).ready(function() {
         $(document).on("click", ".menu-trigger", function(ev) {
