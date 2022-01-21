fcec87fcf4be734bc452f6901f7a804448350bff
Jared Hanson <jaredhanson@gmail.com>
2022-01-21 13:27:19 -0800

    Update site script.

--- site.js
+++ site.js
@@ -11,41 +11,74 @@ var moment = require('moment');
 var site = kerouac();
 site.engine('pug', require('pug'));
 
-site.locals.timeAgo = function(time) {
-  return moment(time).fromNow();
-}
-
-site.set('base url', 'http://www.passportjs.org');
+site.set('base url', 'https://www.passportjs.org');
 site.set('layout engine', 'pug');
 site.locals.pretty = true;
 
-//site.use('/packages', require('kerouac-npm-packages/app/www/site')());
+site.use(kerouac.index());
 
 /*
-site.use('/packages/-/v1', require('kerouac-npm-packages').api.v1());
+//site.use('/packages', require('kerouac-npm-packages/app/www/site')());
+site.use('/packages/-/v1', require('kerouac-npm-packages').api.esc.v1());
 // FIXME: Why can't this be mounted below the /packages?
-site.use('/packages/-', require('kerouac-npm-packages').api());
+site.use('/packages/-', require('kerouac-npm-packages').api.esc());
+//site.use('/packages', require('kerouac-npm-packages').api());
 site.use('/packages', require('kerouac-npm-packages')());
+site.page('/packages/sitemap.xml', require('kerouac-sitemap')());
 */
 
+
+var packagesApp = kerouac();
+packagesApp.locals.pretty = true;
+
+packagesApp.locals.timeAgo = function(time) {
+  return moment(time).fromNow();
+}
+
+packagesApp.set('layout engine', 'pug');
+packagesApp.use(kerouac.index());
+packagesApp.use('/-/v1', require('kerouac-npm-packages').api.esc.v1());
+packagesApp.use('/-', require('kerouac-npm-packages').api.esc());
+packagesApp.use('/', require('kerouac-npm-packages')());
+packagesApp.page('/sitemap.xml', require('kerouac-sitemap')());
+
+site.use('/packages', packagesApp);
+
+
 //site.use('/docs/howto', kerouac.content('howto'));
-site.use('/docs', require('kerouac-book')('Documentation', 'docs', { layout: 'book' }));
+site.use('/concepts/authentication', require('kerouac-book')('Documentation', 'docs', { layout: 'book' }));
+site.use('/concepts/api-authentication', require('kerouac-book')('API Authentication', 'books/concepts/api-authentication', { layout: 'book' }));
+site.use('/concepts/delegated-authorization', require('kerouac-book')('Delegated Authorization', 'books/concepts/delegated-authorization', { layout: 'book' }));
+site.use('/reference/normalized-profile', require('kerouac-book')('Normalized Profile', 'books/reference/normalized-profile', { layout: 'book' }));
+site.use('/tutorials/password', require('kerouac-book')('Documentation', 'books/tutorials/password', { layout: 'book' }));
 site.content('content');
 //site.assets('assets');
 //site.use('/howto', kerouac.content('howto'));
 //site.use('/blog', require('kerouac-blog')({ layout: 'blog' }));
 
 site.use('/', require('kerouac-manifest')());
-//site.page('/sitemap.xml', require('kerouac-sitemap')());
-//site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
-//site.page('/robots.txt', require('kerouac-robotstxt')());
+site.page('/sitemap.xml', require('kerouac-sitemap')());
+site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
+site.page('/robots.txt', require('kerouac-robotstxt')());
 // TODO: .well-known/security.txt
 
-site.generate(function(err) {
-  console.log('DONE!');
-  if (err) {
-    console.error(err.message);
-    console.error(err.stack);
-    return;
-  }
-});
+site.generate([
+    kerouac.contentCrawler(),
+    [ '/concepts/authentication', require('kerouac-book').browser('docs') ],
+    [ '/concepts/api-authentication', require('kerouac-book').browser('books/concepts/api-authentication') ],
+    [ '/concepts/delegated-authorization', require('kerouac-book').browser('books/concepts/delegated-authorization') ],
+    [ '/reference/normalized-profile', require('kerouac-book').browser('books/reference/normalized-profile') ],
+    [ '/tutorials/password', require('kerouac-book').browser('books/tutorials/password') ],
+    //[ '/packages', require('kerouac-npm-packages').browser() ],
+    //[ '/packages', require('kerouac-sitemap').browser(), false ],
+    [ require('kerouac-sitemap').browser({ index: true }), false ],
+    [ require('kerouac-robotstxt').browser(), false ]
+  ],
+  function(err) {
+    console.log('DONE!');
+    if (err) {
+      console.error(err.message);
+      console.error(err.stack);
+      return;
+    }
+  });
