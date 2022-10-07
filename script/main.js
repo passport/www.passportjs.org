edbf5f51e47f91a34ae5bc87f367bb151c99584d
Jared Hanson <jaredhanson@gmail.com>
2022-10-07 15:58:00 -0700

    Add Understanding OAuth 2.0 book.

--- script/main.js
+++ script/main.js
@@ -730,6 +730,7 @@ define("utils", [ "exports" ], function(exports) {
 define("app", [ "./controllers/home", "./controllers/docs", "./controllers/features", "./controllers/packages", "./middleware/controller", "./middleware/highlight", "./middleware/analytics/pageview", "./middleware/ad/refresh", "./shell", "./utils", "page", "jquery" ], function(homeController, docsController, featuresController, packagesController, controller, highlight, pageview, adRefresh, shell, utils, page, $) {
     page("/", controller(homeController), pageview(), adRefresh());
     page("/concepts/authentication/*", controller(docsController), pageview(), adRefresh());
+    page("/concepts/oauth2/*", highlight());
     page("/tutorials/*", highlight());
     page("/howtos/*", highlight());
     page("/features", controller(featuresController), pageview(), adRefresh());
