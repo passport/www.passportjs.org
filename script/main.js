62dc8754cea9cf2b2acfe01108d8f01ec11e86a5
Jared Hanson <jaredhanson@gmail.com>
2022-11-18 13:10:28 -0800

    Update code highlighting and styles.

--- script/main.js
+++ script/main.js
@@ -730,9 +730,6 @@ define("utils", [ "exports" ], function(exports) {
 define("app", [ "./controllers/home", "./controllers/docs", "./controllers/features", "./controllers/packages", "./middleware/controller", "./middleware/highlight", "./middleware/analytics/pageview", "./middleware/ad/refresh", "./shell", "./utils", "page", "jquery" ], function(homeController, docsController, featuresController, packagesController, controller, highlight, pageview, adRefresh, shell, utils, page, $) {
     page("/", controller(homeController), pageview(), adRefresh());
     page("/concepts/authentication/*", controller(docsController), pageview(), adRefresh());
-    page("/concepts/oauth2/*", highlight());
-    page("/tutorials/*", highlight());
-    page("/howtos/*", highlight());
     page("/features", controller(featuresController), pageview(), adRefresh());
     page("/packages", controller(packagesController, true), pageview());
     $(document).ready(function() {
